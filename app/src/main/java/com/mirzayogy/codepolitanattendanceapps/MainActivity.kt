2591cc72b25dec5fbdcdf1ac12153942c82dc1e8
amukancabbage <amukancabbage@gmail.com>
2020-11-04 12:15:38 +0800

    Desain Aplikasi

--- app/src/main/java/com/mirzayogy/codepolitanattendanceapps/MainActivity.kt
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.mirzayogy.codepolitanattendanceapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.skyfishjy.library.RippleBackground
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        onClick()
+    }
+
+
+    private fun onClick(){
+        fabCheckIn.setOnClickListener{
+            loadScanLocation()
+            Handler(Looper.getMainLooper()).postDelayed({
+                stopScanLocation()
+            },4000)
+        }
+    }
+
+    private fun loadScanLocation(){
+        rippleBackground.startRippleAnimation()
+        textViewScanning.visibility = View.VISIBLE
+        textViewCheckInSuccess.visibility = View.GONE
+    }
+
+    private fun stopScanLocation(){
+        rippleBackground.stopRippleAnimation()
+        textViewScanning.visibility = View.GONE
     }
 }
