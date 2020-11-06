b0abbbbb99d043f5bea4677bdff6f79131009f04
amukancabbage <amukancabbage@gmail.com>
2020-11-06 21:10:22 +0800

    Desain Auth

---
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/SignInActivity.kt
@@ -0,0 +1,20 @@
+package com.mirzayogy.codepolitanattendanceapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(tbSignIn)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+    }
+}
---
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/SignUpActivity.kt
@@ -0,0 +1,20 @@
+package com.mirzayogy.codepolitanattendanceapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(tbSignUp)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+    }
+}
