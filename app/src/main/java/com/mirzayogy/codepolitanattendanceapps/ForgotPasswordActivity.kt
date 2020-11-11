2034d24cb0e6cea24a9f69dece2d30c1471debb3
amukancabbage <amukancabbage@gmail.com>
2020-11-11 22:45:56 +0800

    ForgotPassword

--- app/src/main/java/com/mirzayogy/codepolitanattendanceapps/ForgotPasswordActivity.kt
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/ForgotPasswordActivity.kt
@@ -3,7 +3,9 @@ package com.mirzayogy.codepolitanattendanceapps
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -17,12 +19,39 @@ class ForgotPasswordActivity : AppCompatActivity() {
         initActionBar()
 
         btnSendEmail.setOnClickListener {
+            val email = editTextEmailForgot.text.toString().trim()
+            if(email.isEmpty()){
+                editTextEmailForgot.error = getString(R.string.please_fill_in_your_email)
+                editTextEmailForgot.requestFocus()
+            }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                editTextEmailForgot.error = getString(R.string.please_use_valid_email)
+                editTextEmailForgot.requestFocus()
+            }else{
+                forgotPassword(email)
+            }
             Toast.makeText(this,getString(R.string.send_email),Toast.LENGTH_SHORT).show()
         }
 
         tbForgotPassword.setNavigationOnClickListener {
             finish()
         }
+    }
+
+    private fun forgotPassword(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    Toast.makeText(this, "Sent to email",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,SignInActivity::class.java))
+                    finishAffinity()
+                }else{
+                    Toast.makeText(this, "Reset failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+
+            }
     }
 
     private fun initActionBar() {
