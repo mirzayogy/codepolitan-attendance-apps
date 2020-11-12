5fa7aa2df86b8373cd013c500de519865fe644b9
amukancabbage <amukancabbage@gmail.com>
2020-11-12 10:21:29 +0800

    SignIn with Google

--- app/src/main/java/com/mirzayogy/codepolitanattendanceapps/DashboardActivity.kt
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/DashboardActivity.kt
@@ -1,14 +1,20 @@
 package com.mirzayogy.codepolitanattendanceapps
 
+import android.content.ContentProviderClient
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var auth : FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,11 @@ class DashboardActivity : AppCompatActivity() {
 
         btnLogout.setOnClickListener {
             auth.signOut()
+
+            mGoogleSignInClient.signOut().addOnCompleteListener {
+                Toast.makeText(this, getString(R.string.succes_fully_signed_out),Toast.LENGTH_SHORT).show()
+            }
+
             startActivity(Intent(this,AuthActivity::class.java))
             finish()
         }
@@ -34,5 +45,12 @@ class DashboardActivity : AppCompatActivity() {
 
     private fun initFirebaseAuth() {
         auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
     }
 }
--- app/src/main/java/com/mirzayogy/codepolitanattendanceapps/SignInActivity.kt
+++ app/src/main/java/com/mirzayogy/codepolitanattendanceapps/SignInActivity.kt
@@ -5,15 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import java.util.regex.Pattern
 
 class SignInActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    companion object{
+        const val RC_SIGN_IN = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +47,35 @@ class SignInActivity : AppCompatActivity() {
             startActivity(i)
         }
 
+        btnGoogleSignIn.setOnClickListener{
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+
         tbSignIn.setNavigationOnClickListener {
             finish()
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            CustomDialog.showLoading(this)
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try{
+                val account = task.getResult(ApiException::class.java)
+                val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+                firebaseAuth(credential)
+
+            }catch (e: ApiException){
+                CustomDialog.hideLoading()
+                Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 
     private fun loginToServer(email: String, password: String) {
@@ -86,6 +121,13 @@ class SignInActivity : AppCompatActivity() {
 
     private fun initFirebaseAuth() {
         auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
     }
 
     private fun initActionBar() {
@@ -94,3 +136,8 @@ class SignInActivity : AppCompatActivity() {
         supportActionBar?.title = ""
     }
 }
+
+//firebase -> authentication -> sign-in method -> google
+// ./gradlew signingReport
+//9B:D4:76:BE:59:AE:1F:B9:21:25:3E:50:96:BE:1F:94:87:3F:39:D4
+//firebase -> project setting -> signing SHA certificate
